a54242ea45266d0302632604ce5b7261fbba09b1
Vignesh Balaji <vigneshbalaji@wlan095230.mobiel.utwente.nl>
2022-10-01 20:29:59 +0200

    resolve merge conflict

--- atlas_scratch.py
+++ atlas_scratch.py
@@ -9,11 +9,16 @@ from python.pybullet_api.data import PyBulletData
 
 def startup_env():
 	PyBulletSimulator()
+<<<<<<< HEAD
 
 	atlas = p.loadURDF("/Users/vigneshbalaji/atlas-humanoid-robot/data/atlas/atlas_v4_with_multisense.urdf", [-2,3,-0.5])
 
 	while(1):
 		p.getConnectionInfo()
+=======
+#	while(1):                #  write a step function, example just defining simulation timestep 
+#		print("good")
+>>>>>>> 12e6922 (rebased with master)
 
 
 
